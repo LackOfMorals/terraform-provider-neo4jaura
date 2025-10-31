ed93029a4395c93d74c09013e09cf03f09310c14
Eugene Rubanov <eugene.rubanov@neo4j.com>
2025-10-31 15:05:10 +0000

    feat: tools

---
+++ tools/go.mod
@@ -0,0 +1,7 @@
+module tools
+
+go 1.25
+
+require (
+	github.com/hashicorp/terraform-plugin-docs v0.24.0
+)
---
+++ tools/tools.go
@@ -0,0 +1,13 @@
+//go:build generate
+
+package tools
+
+import (
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+)
+
+// Format terraform files in examples
+//go:generate terraform fmt -recursive ../examples/
+
+// Generate documentation
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. --provider-name neo4jaura
